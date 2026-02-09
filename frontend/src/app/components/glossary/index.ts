d5176bf05e2609a1ec4bef114b03c9b60a2fd9ab
uouake <uriel.ouake@gmail.com>
2026-02-09 12:46:29 +0100

    fix: export type for GlossaryTerm

--- frontend/src/app/components/glossary/index.ts
+++ frontend/src/app/components/glossary/index.ts
@@ -1,5 +1,6 @@
 // Glossary module exports
-export { GlossaryService, GlossaryTerm } from '../../core/glossary.service';
+export { GlossaryService } from '../../core/glossary.service';
+export type { GlossaryTerm } from '../../core/glossary.service';
 export { GlossaryTooltipComponent } from './glossary-tooltip.component';
 export { GlossaryTooltipPopupComponent } from './glossary-tooltip-popup.component';
 export { GlossaryHandlerDirective } from './glossary-handler.directive';
